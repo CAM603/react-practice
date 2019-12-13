b1ed0d6791dff4c4c7c312fdc98a9b269e00cc67
CAM603 <cameronhawley@comcast.net>
2019-12-13 09:09:25 -0500

    added form

--- src/Dogs.js
+++ src/Dogs.js
@@ -4,12 +4,25 @@ import DogCard from "./DogCard";
 const Dogs = props => {
 
     const { dogs, changeBreed } = props;
-    
-    
+
+    const [breed, setBreed] = useState({
+        breed : ''
+    });
+    const onSubmit = event => {
+        event.preventDefault();
+        changeBreed(breed.breed)
+    }
+    const handleChange = (event) => {
+        setBreed({[event.target.name]: event.target.value})
+    }
+    console.log(breed);
     return (
         <div>
             <div>
-                <button onClick={()=>changeBreed('pug')}>Change to Pug</button>
+                <form onSubmit={e => onSubmit(e)}>
+                    <input type="text" name="breed" onChange={handleChange}></input>
+                <button>Change Breed</button>
+                </form>
             </div>
             <div>
             {dogs.map((dog, index) => {
